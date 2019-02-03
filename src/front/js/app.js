e7b3f3498a3f6fa4b86d940e12a7bf920ab76b7f
airtonbjunior <airtonbjunior@gmail.com>
2019-02-03 00:16:36 -0200

    some logs on main screen

--- src/front/js/app.js
+++ src/front/js/app.js
@@ -3,6 +3,12 @@
 */
 
 function searchFn() {
+	content = document.getElementById("search-input").value
+	
+	if (content.length == 0) {
+		alert("Type some topic")
+		return
+	}
 
-	alert("I'll search about " + document.getElementById("search-input").value)
+	alert("I'll search about " + content)
 }
